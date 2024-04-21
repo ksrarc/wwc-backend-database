b7df553e4e0860a7823b8ee5c17b7c800f2e5190
Cesar Augusto Arana Collazos <ksrarc@gmail.com>
2024-04-20 20:17:06 -0500

    creacion express app

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.info(`listening on port: ${port}`);
+});
